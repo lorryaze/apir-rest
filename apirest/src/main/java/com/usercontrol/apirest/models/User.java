3515c72d46d0eeffd1b091e6646fcdbccbd1bbba
lorryaze <lorrany90@gmail.com>
2020-02-04 15:35:26 -0300

    Api rest

---
+++ apirest/src/main/java/com/usercontrol/apirest/models/User.java
@@ -0,0 +1,53 @@
+package com.usercontrol.apirest.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name="TB_USER")
+public class User implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private long id;
+	private String email;
+	private String senha;
+	private boolean perfil;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Boolean getPerfil() {
+		return perfil;
+	}
+	public void setPerfil(boolean perfil) {
+		this.perfil = perfil;
+	}
+
+}
