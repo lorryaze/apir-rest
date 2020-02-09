9d16f0e59bf79504f217aa1d261dad4a6ab935af
lorryaze <lorrany90@gmail.com>
2020-02-09 04:08:36 -0300

    My version

--- apirest/src/main/java/com/usercontrol/apirest/config/SwaggerConfig.java
+++ apirest/src/main/java/com/usercontrol/apirest/config/SwaggerConfig.java
@@ -25,16 +25,16 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.SecurityConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-//import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 
 //import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
-@EnableSwagger2
-//@EnableSwagger2WebMvc
+//@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
 	private static final String CLIENT_ID = "admin";
 
